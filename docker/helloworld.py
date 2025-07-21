9b907017c4d25d2c73a707f68be4f2d41d9e99bc
sanjeevpatil804 <sanjeevpatil804@aol.com>
2025-07-21 16:00:24 +0200

    1

---
+++ docker/helloworld.py
@@ -0,0 +1,11 @@
+from crypt import methods
+from flask import Flask
+import os
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    return "Hello, World!"
+
+if __name__=='__main__':
+    app.run(debug=True,host='0.0.0.0',port=8000)
